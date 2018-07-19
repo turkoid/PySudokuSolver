a7264512c931a90cd1e2934cc66eb7058712179a
John Ross <jlross42@gmail.com>
2018-07-19 09:40:20 -0500

    popuplate marks + first part of solver

--- sudoku.py
+++ sudoku.py
@@ -26,7 +26,7 @@ class SudokuBoard(object):
 
         self.rows = [self.row(y) for y in range(0, height * 3)]
         self.columns = [self.column(x) for x in range(0, width * 3)]
-        self.squares = [self.square(Point(x, y), True) for y in range(0, height) for x in range(0, width)]
+        self.squares = [self.square(Point(x, y)) for y in range(0, height) for x in range(0, width)]
 
     def row(self, y):
         return self.board[y]
@@ -34,7 +34,7 @@ class SudokuBoard(object):
     def column(self, x):
         return self.board[:, x].reshape(self.height * 3)
 
-    def square(self, coord, flat=False):
+    def square(self, coord, flat=True):
         x = coord.x * 3
         y = coord.y * 3
         sq = self.board[y:y + 3, x:x + 3]
@@ -61,17 +61,45 @@ class SudokuBoard(object):
     def coord_to_index(self, coord):
         return coord.y * (self.height * 3) + coord.x
 
+    def populate_marks(self):
+        for row in self.board:
+            for cell in row:
+                if not cell.is_initial:
+                    cell.reset()
+                    test_cells = [c for c in self.square(cell.square) if c is not cell]
+                    test_cells += [c for c in self.column(cell.coordinate.x) if c is not cell and c.square.y != cell.square.y]
+                    test_cells += [c for c in self.row(cell.coordinate.y) if c is not cell and c.square.x != cell.square.x]
+                    for mark in range(1, 10):
+                        add_mark = True
+                        for test_cell in test_cells:
+                            if test_cell is not cell and test_cell == mark:
+                                add_mark = False
+                                break
+                        if add_mark:
+                            # oh hai mark
+                            cell.pencil_marks[mark - 1] = mark
+
+    def solve(self):
+        # first set the value for any cell that has only one valid option
+        for row in self.board:
+            for cell in row:
+                values = [mark for mark in cell.pencil_marks if mark != 0]
+                if cell.value == 0 and len(values) == 1:
+                    cell.set_value(values[0])
+
 
 class Cell(object):
-    pencil_marks = None
+    pencil_marks = []
     value = 0
     coordinate = None
+    square = None
+    is_initial = False
 
-    def __init__(self, coord, value=None):
-        self.pencil_marks = np.zeros(9, dtype=int)
-        if value is not None and int(value) > 0:
-            self.value = int(value)
-            self.pencil_marks[self.value - 1] = self.value
+    def __init__(self, coordinate, value=None):
+        self.coordinate = coordinate
+        self.square = Point(int(coordinate.x / 3), int(coordinate.y / 3))
+        self.reset()
+        self.is_initial = self.set_value(value)
 
     def display(self, zero=True):
         return " " if not zero and self.value == 0 else str(self.value)
@@ -82,11 +110,21 @@ class Cell(object):
     def set_value(self, value):
         if value is not None and int(value) > 0:
             self.value = int(value)
-            self.pencil_marks = np.zeros(9, dtype=int)
+            self.reset()
             self.pencil_marks[self.value - 1] = self.value
+            return True
+        return False
 
-    def solve(self):
-        pass
+    def reset(self):
+        self.pencil_marks = [0] * 9
+
+    def __eq__(self, other):
+        if isinstance(other, Cell):
+            return self.value == other.value
+        elif isinstance(other, list):
+            return self.pencil_marks == other
+        else:
+            return self.value == other
 
 
 s = (
@@ -105,4 +143,4 @@ board = SudokuBoard(initial=s)
 # print(board.column(7))
 # print(board.square(0, 1, True))
 print(board)
-print(board.squares)
+board.populate_marks()
