5c64df24e7846df3adcee5ef0810bf4a2cace831
John Ross <jlross42@gmail.com>
2018-07-21 10:44:51 -0500

    rename 'linked' to 'related'

--- sudoku.py
+++ sudoku.py
@@ -82,7 +82,7 @@ class SudokuBoard(object):
                     cell.reset()
                     for mark in range(1, 10):
                         add_mark = True
-                        for test_cell in self.get_linked_cells(cell):
+                        for test_cell in self.get_related_cells(cell):
                             if test_cell is not cell and test_cell.value == mark:
                                 add_mark = False
                                 break
@@ -107,26 +107,26 @@ class SudokuBoard(object):
     @staticmethod
     def filter_cells(cells, filter, func=""):
         changed = False
-        for linked_cell in cells:
-            if linked_cell.remove_pencil_marks(filter):
+        for related_cell in cells:
+            if related_cell.remove_pencil_marks(filter):
                 changed = True
-                print("%s: %s - %s" % (func, SudokuBoard.print_coord(linked_cell.coordinate), filter))
+                print("%s: %s - %s" % (func, SudokuBoard.print_coord(related_cell.coordinate), filter))
         return changed
 
-    def get_linked_cells(self, cell, relations=["square", "row", "column"], include_solved=True, excluded_cells=[]):
-        linked_cells = []
+    def get_related_cells(self, cell, relations=["square", "row", "column"], include_solved=True, excluded_cells=[]):
+        related_cells = []
         if "square" in relations:
-            linked_cells += [c for c in self.square(cell.square)
+            related_cells += [c for c in self.square(cell.square)
                              if c is not cell and c not in excluded_cells and (include_solved or c.value == 0)]
         if "row" in relations:
-            linked_cells += [c for c in self.row(cell.coordinate.y)
+            related_cells += [c for c in self.row(cell.coordinate.y)
                              if c is not cell and c not in excluded_cells and (include_solved or c.value == 0)
                              and ("square" not in relations or c.square != cell.square)]
         if "column" in relations:
-            linked_cells += [c for c in self.column(cell.coordinate.x)
+            related_cells += [c for c in self.column(cell.coordinate.x)
                              if c is not cell and c not in excluded_cells and (include_solved or c.value == 0)
                              and ("square" not in relations or c.square != cell.square)]
-        return linked_cells
+        return related_cells
 
     def check_sole_candidates(self):
         changed = False
@@ -136,7 +136,7 @@ class SudokuBoard(object):
             if len(values) == 1:
                 changed = True
                 cell.set_value(values[0])
-                SudokuBoard.filter_cells(self.get_linked_cells(cell, include_solved=False), [cell.value], "sole_candidates")
+                SudokuBoard.filter_cells(self.get_related_cells(cell, include_solved=False), [cell.value], "sole_candidates")
         return changed
 
     def check_unique_candidates(self):
@@ -145,18 +145,18 @@ class SudokuBoard(object):
         for cell in cells:
             is_cell_changed = False
             for relation in ["square", "row", "column"]:
-                linked_cells = self.get_linked_cells(cell, [relation], include_solved=False)
+                related_cells = self.get_related_cells(cell, [relation], include_solved=False)
                 marks = [mark for mark in cell.pencil_marks if mark != 0]
                 for mark in marks:
                     is_unique = True
-                    for linked_cell in linked_cells:
-                        if linked_cell.pencil_marks[mark - 1] == mark:
+                    for related_cell in related_cells:
+                        if related_cell.pencil_marks[mark - 1] == mark:
                             is_unique = False
                             break
-                    if is_unique and len(linked_cells) > 0:
+                    if is_unique and len(related_cells) > 0:
                         cell.set_value(mark)
                         is_cell_changed = True
-                        SudokuBoard.filter_cells(self.get_linked_cells(cell, include_solved=False), [cell.value], "unique_candidates")
+                        SudokuBoard.filter_cells(self.get_related_cells(cell, include_solved=False), [cell.value], "unique_candidates")
                         break
                 if is_cell_changed:
                     break
@@ -180,7 +180,7 @@ class SudokuBoard(object):
                     for combo in combinations:
                         unique_marks = set().union(*[combo_cell.pencil_marks for combo_cell in combo if combo_cell.value == 0]) - {0}
                         if len(unique_marks) == combo_length:
-                            changed = SudokuBoard.filter_cells(self.get_linked_cells(combo[0], [relation], False, list(combo)), list(unique_marks), "subsets") or changed
+                            changed = SudokuBoard.filter_cells(self.get_related_cells(combo[0], [relation], False, list(combo)), list(unique_marks), "subsets") or changed
         return changed
 
     def check_blocking_subsets(self):
@@ -193,15 +193,38 @@ class SudokuBoard(object):
 
             for cell_group in cell_groups:
                 for link_relation in [relation, "square"]:
-                    linked_cells = self.get_linked_cells(cell_group[0], [link_relation], False, list(cell_group))
+                    related_cells = self.get_related_cells(cell_group[0], [link_relation], False, list(cell_group))
                     cell_group_marks = set().union(*[cell.pencil_marks for cell in cell_group if cell.value == 0]) - {0}
-                    linked_group_marks = set().union(*[cell.pencil_marks for cell in linked_cells if cell.value == 0]) - {0}
-                    blocking_marks = cell_group_marks - linked_group_marks
+                    related_group_marks = set().union(*[cell.pencil_marks for cell in related_cells if cell.value == 0]) - {0}
+                    blocking_marks = cell_group_marks - related_group_marks
                     if len(blocking_marks) > 0:
-                        filter_cells = self.get_linked_cells(cell_group[0], [relation, "square"], False, list(cell_group))
+                        filter_cells = self.get_related_cells(cell_group[0], [relation, "square"], False, list(cell_group))
                         changed = SudokuBoard.filter_cells(filter_cells, list(blocking_marks), "blocking_subsets") or changed
         return changed
 
+    def check_x_wing(self):
+        changed = False
+        for relation in ["square", "row", "column"]:
+            if relation == "square":
+                cell_groups = self.squares
+            elif relation == "row":
+                cell_groups = self.rows
+            elif relation == "column":
+                cell_groups = self.columns
+
+            for cell_group in cell_groups:
+                cells = [cell for cell in cell_group if cell.value == 0]
+                for combo_length in range(2, len(cells) - 1):
+                    combinations = it.combinations(cells, combo_length)
+                    for combo in combinations:
+                        unique_marks = set().union(
+                            *[combo_cell.pencil_marks for combo_cell in combo if combo_cell.value == 0]) - {0}
+                        if len(unique_marks) == combo_length:
+                            changed = SudokuBoard.filter_cells(
+                                self.get_related_cells(combo[0], [relation], False, list(combo)), list(unique_marks),
+                                "subsets") or changed
+        return changed
+    
     @staticmethod
     def print_cells_coordinates(cells):
         return [SudokuBoard.print_coord(c.coordinate) for c in cells]
@@ -284,6 +307,8 @@ initial = (
     "020900605"
     )
 #initial = "070004000869000000000000010000010007080009600002057040958003000000001200300000789"
+initial = "090600800000503400807000610000050007000790100000006300070000020040000000203061004"
+initial = "000000000200601005004203900031000850600705009085000470006509200400106007000000000" #4831
 board = SudokuBoard(initial)
 print(board)
 board.solve()
